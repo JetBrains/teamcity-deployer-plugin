082b5450ea1fcb4fea2cb9d6f59d01d004edc80e
jb-demo <nikita.skvortsov@jetbrains.com>
2012-10-04 20:16:14 +0400

    ability to re-deploy existing applications in Tomcat runner

--- deploy-runner/deploy-runner-agent/src/main/java/jetbrains/buildServer/deployer/agent/tomcat/TomcatBuildProcessAdapter.java
+++ deploy-runner/deploy-runner-agent/src/main/java/jetbrains/buildServer/deployer/agent/tomcat/TomcatBuildProcessAdapter.java
@@ -5,6 +5,9 @@ import jetbrains.buildServer.agent.BuildFinishedStatus;
 import jetbrains.buildServer.agent.BuildProcessAdapter;
 import jetbrains.buildServer.agent.BuildRunnerContext;
 import org.apache.catalina.ant.DeployTask;
+import org.apache.catalina.ant.ListTask;
+import org.apache.catalina.ant.UndeployTask;
+import org.apache.tools.ant.Project;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -12,22 +15,24 @@ import java.io.File;
 
 class TomcatBuildProcessAdapter extends BuildProcessAdapter {
 
-    private static final String MANAGER_APP_SUFFIX = "manager/html";
+    private static final String MANAGER_APP_SUFFIX = "manager";
     private static final String HTTP_PREFIX = "http://";
-    private final String target;
-    private final String username;
-    private final String password;
-    private final BuildRunnerContext context;
-    private final String sourcePath;
+    private static final String CONTEXTS_LIST_PROPERTY = "temp.contexts.list";
+
+    private final String myTarget;
+    private final String myUsername;
+    private final String myPassword;
+    private final BuildRunnerContext myContext;
+    private final String myWarPath;
 
     private volatile boolean hasFinished;
 
     public TomcatBuildProcessAdapter(String target, String username, String password, BuildRunnerContext context, String sourcePath) {
-        this.target = target;
-        this.username = username;
-        this.password = password;
-        this.context = context;
-        this.sourcePath = sourcePath;
+        myTarget = target;
+        myUsername = username;
+        myPassword = password;
+        myContext = context;
+        myWarPath = sourcePath;
         hasFinished = false;
     }
 
@@ -50,32 +55,51 @@ class TomcatBuildProcessAdapter extends BuildProcessAdapter {
 
         try {
             final StringBuilder sb = new StringBuilder();
-            if (!target.startsWith(HTTP_PREFIX)) {
+            if (!myTarget.startsWith(HTTP_PREFIX)) {
                 sb.append(HTTP_PREFIX);
             }
-            sb.append(target);
+            sb.append(myTarget);
 
-            if (!target.endsWith(MANAGER_APP_SUFFIX)) { // path to manager/html is required
-                if (!target.endsWith("/")) {
+            if (!myTarget.endsWith(MANAGER_APP_SUFFIX)) { // path to manager/html is required
+                if (!myTarget.endsWith("/")) {
                     sb.append('/');
                 }
                 sb.append(MANAGER_APP_SUFFIX);
             }
 
-            final String targetUrl = sb.toString();
-            DeployTask deployTask = new DeployTask();
-            deployTask.setUrl(targetUrl);
-            deployTask.setUsername(username);
-            deployTask.setPassword(password);
-
-            final File deployableWar = new File(context.getWorkingDirectory(), sourcePath);
-            deployTask.setWar(deployableWar.getCanonicalPath());
-
+            final File deployableWar = new File(myContext.getWorkingDirectory(), myWarPath);
             final String fileNameWithExtension = deployableWar.getName();
             final String contextName = fileNameWithExtension.substring(0, fileNameWithExtension.length() - 4);
+            final String targetUrl = sb.toString();
 
+            final Project tempProject = new Project();
+            final ListTask list = new ListTask();
+            list.setProject(tempProject);
+            list.setUrl(targetUrl);
+            list.setUsername(myUsername);
+            list.setPassword(myPassword);
+            list.setOutputproperty(CONTEXTS_LIST_PROPERTY);
+            list.execute();
+
+            final String contextsList = tempProject.getProperty(CONTEXTS_LIST_PROPERTY);
+
+            if (contextsList.contains(contextName)) {
+                final UndeployTask undeployTask = new UndeployTask();
+                undeployTask.setUrl(targetUrl);
+                undeployTask.setUsername(myUsername);
+                undeployTask.setPassword(myPassword);
+                undeployTask.setPath("/" + contextName);
+                undeployTask.execute();
+            }
+
+            final DeployTask deployTask = new DeployTask();
+            deployTask.setUrl(targetUrl);
+            deployTask.setUsername(myUsername);
+            deployTask.setPassword(myPassword);
+            deployTask.setWar(deployableWar.getCanonicalPath());
             deployTask.setPath("/" + contextName);   // leading / is required!
             deployTask.execute();
+
         } catch (Exception e) {
             throw new RunBuildException(e);
         } finally {
