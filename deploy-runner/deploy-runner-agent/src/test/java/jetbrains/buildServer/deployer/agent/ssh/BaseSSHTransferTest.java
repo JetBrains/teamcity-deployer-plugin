c8680e72ab5122f5eb49448f2f05ce0572e2b2ac
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2013-02-15 16:12:30 +0400

    fix absolute destination path tests for MS Windows. Such paths should have leading "/" to be usable.

--- deploy-runner/deploy-runner-agent/src/test/java/jetbrains/buildServer/deployer/agent/ssh/BaseSSHTransferTest.java
+++ deploy-runner/deploy-runner-agent/src/test/java/jetbrains/buildServer/deployer/agent/ssh/BaseSSHTransferTest.java
@@ -151,11 +151,23 @@ public abstract class BaseSSHTransferTest {
         final String subPath = "test_path/subdir";
         final File tempDir1 = myTempFiles.createTempDir();
         final File tempDir2 = myTempFiles.createTempDir();
-        myArtifactsCollections.add(DeployTestUtils.buildArtifactsCollection(myTempFiles, new File(tempDir1, "dest1").getCanonicalPath(), new File(tempDir1, "dest2").getCanonicalPath(), new File(tempDir2, "dest3").getCanonicalPath()));
+        final String dest1 = prependLeadingSlash(new File(tempDir1, "dest1").getCanonicalPath());
+        final String dest2 = prependLeadingSlash(new File(tempDir1, "dest2").getCanonicalPath());
+        final String dest3 = prependLeadingSlash(new File(tempDir2, "dest3").getCanonicalPath());
+        myArtifactsCollections.add(DeployTestUtils.buildArtifactsCollection(myTempFiles, dest1, dest2, dest3));
         final BuildProcess process = getProcess(HOST_ADDR + ":" + subPath);
         DeployTestUtils.runProcess(process, 5000);
         DeployTestUtils.assertCollectionsTransferred(myRemoteDir, myArtifactsCollections);
     }
+
+    protected String prependLeadingSlash(String path) {
+        if (!path.startsWith("/")) {
+            return "/" + path;
+        } else {
+            return path;
+        }
+    }
+
 
     @Test
     public void testTransferToExistingPath() throws Exception {
