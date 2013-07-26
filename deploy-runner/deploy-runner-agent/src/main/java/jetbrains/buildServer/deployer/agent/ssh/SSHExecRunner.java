2502d61d6bf64ed163995fc0ea42605c87e36ac1
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2013-07-26 15:43:11 +0400

    fix http://youtrack.jetbrains.com/issue/TW-28631 replace null with empty string

--- deploy-runner/deploy-runner-agent/src/main/java/jetbrains/buildServer/deployer/agent/ssh/SSHExecRunner.java
+++ deploy-runner/deploy-runner-agent/src/main/java/jetbrains/buildServer/deployer/agent/ssh/SSHExecRunner.java
@@ -3,12 +3,9 @@ package jetbrains.buildServer.deployer.agent.ssh;
 import com.jcraft.jsch.JSchException;
 import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.*;
-import jetbrains.buildServer.deployer.common.DeployerRunnerConstants;
 import jetbrains.buildServer.deployer.common.SSHRunnerConstants;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
 
 public class SSHExecRunner implements AgentBuildRunner {
 
@@ -23,7 +20,7 @@ public class SSHExecRunner implements AgentBuildRunner {
             throw new RunBuildException(e);
         }
 
-        final String command = context.getRunnerParameters().get(SSHRunnerConstants.PARAM_COMMAND);
+        final String command = StringUtil.notNullize(context.getRunnerParameters().get(SSHRunnerConstants.PARAM_COMMAND));
         return new SSHExecProcessAdapter(provider, command, runningBuild.getBuildLogger());
     }
 
