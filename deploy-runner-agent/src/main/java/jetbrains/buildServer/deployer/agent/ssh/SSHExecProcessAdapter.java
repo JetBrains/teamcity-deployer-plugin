a1eca1c1232599070067c50770ac2778172941a3
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2013-10-30 15:04:17 +0400

    make ssh exec interruptible

--- deploy-runner-agent/src/main/java/jetbrains/buildServer/deployer/agent/ssh/SSHExecProcessAdapter.java
+++ deploy-runner-agent/src/main/java/jetbrains/buildServer/deployer/agent/ssh/SSHExecProcessAdapter.java
@@ -1,7 +1,6 @@
 package jetbrains.buildServer.deployer.agent.ssh;
 
 import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.BuildProgressLogger;
@@ -9,6 +8,7 @@ import jetbrains.buildServer.deployer.agent.SyncBuildProcessAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 
@@ -52,27 +52,37 @@ class SSHExecProcessAdapter extends SyncBuildProcessAdapter {
             channel = (ChannelExec)session.openChannel("exec");
             channel.setCommand(command);
 
-            final BufferedReader reader = new BufferedReader(new InputStreamReader(channel.getInputStream()));
-            channel.connect();
-
+            final InputStream inputStream = channel.getInputStream();
             final StringBuilder result = new StringBuilder();
-            while (true) {
-                final String str = reader.readLine();
-                if (str != null) {
-                    result.append(str).append('\n');
-                } else {
+            byte[] buf = new byte[8192];
+
+            channel.connect();
+            while (!isInterrupted()) {
+                if (inputStream.available() > 0) {
+                    int i = inputStream.read(buf, 0, 8192);
+                    if (i < 0) {
+                        break;
+                    }
+                    result.append(new String(buf, 0, i));
+                }
+                if (channel.isClosed()) {
                     break;
                 }
             }
+
             myLogger.message("Exec output:\n" + result.toString());
+            if (isInterrupted()) {
+                myLogger.message("Interrupted.");
+            }
+
         } finally {
             if (channel != null) {
                 channel.disconnect();
                 int exitCode = channel.getExitStatus();
+                myLogger.message("ssh exit-code: " + exitCode);
                 if (exitCode > 0) {
                     throw new RunBuildException("Non-zero exit code from ssh exec: [" + exitCode + "]");
                 }
-                myLogger.message("ssh exit-code: " + exitCode);
             }
         }
 
