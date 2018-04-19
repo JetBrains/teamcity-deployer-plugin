9b924b24c608757019ba86d8c30591ac35a79ba6
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2018-04-19 08:44:45 +0300

    update description of old runner

--- deploy-runner-server/src/main/java/jetbrains/buildServer/deployer/server/SmbDeployerRunType.java
+++ deploy-runner-server/src/main/java/jetbrains/buildServer/deployer/server/SmbDeployerRunType.java
@@ -41,7 +41,7 @@ public class SmbDeployerRunType extends RunType {
   @NotNull
   @Override
   public String getDescription() {
-    return "Deploys files/directories via SMB (Windows share)";
+    return "Deploys files/directories via SMB v1 (old windows share protocol)";
   }
 
   @Override
