b0c984f9d7f8c64cc4d432d11f78a523320f396a
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2018-04-20 10:00:11 +0300

    fix creation of top level directory

--- deploy-runner-agent-smb2/src/main/java/jetbrains/buildServer/deployer/agent/smb/SMBJBuildProcessAdapter.java
+++ deploy-runner-agent-smb2/src/main/java/jetbrains/buildServer/deployer/agent/smb/SMBJBuildProcessAdapter.java
@@ -139,7 +139,7 @@ public class SMBJBuildProcessAdapter extends SyncBuildProcessAdapter {
     }
 
     while (!toCreate.empty()) {
-      existingPrefix = existingPrefix + "\\" + toCreate.pop();
+      existingPrefix = (existingPrefix.length() > 0 ? existingPrefix + "\\" : "") + toCreate.pop();
       diskShare.mkdir(existingPrefix);
     }
   }
